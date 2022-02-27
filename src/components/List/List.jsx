d436a4e40665b6e7621c0f0a6e243f63ec60e444
Milan <mayowam52@gmail.com>
2022-02-27 19:30:36 +0100

    First stage of header done

---
+++ src/components/List/List.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from '@material-ui/core';
+
+import useStyles from './styles';
+
+
+const List = () => {
+    const classes = useStyles();
+    const [type, setType] = useState('restaurants');
+
+    return (
+        <div className={classes.container}>
+            <Typography variant="h4">
+                Restaurants, Hotels & Attractions around you
+            <FormControl className={classes.formControl}>
+                <InputLabel>Type</InputLabel>
+                <Select value={} onChange={}>
+                    <MenuItem value="restaurants">Restaurants</MenuItem>
+                    <MenuItem value="hotels">Hotels</MenuItem>
+                    <MenuItem value="attractions">Attractions</MenuItem>
+                </Select>
+            </FormControl>
+            </Typography>
+        </div>
+    );
+}
+
+export default List;
